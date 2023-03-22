ca8f8ff0a50a88d9fb24aef9c3d2b64725083d62
Jason Uwaeze <34732790+Wazhee@users.noreply.github.com>
2023-03-22 17:14:39 -0500

    Update LLE.py

--- Scripts/LLE.py
+++ Scripts/LLE.py
@@ -7,6 +7,9 @@ from sklearn.manifold import LocallyLinearEmbedding as LLE
 from sklearn.manifold import locally_linear_embedding as MLLE
 from PIL import Image
 
+"""
+Initialize Model
+"""
 DR_components=2
 which_modality=[0,1,2,3] # pdw,flair,t2,pre
 M=len(which_modality)
@@ -20,13 +23,22 @@ print(np.shape(data))
 print(np.shape(vec))
 
 model = MLLE(vec, n_components=DR_components,n_neighbors=70)
+
+
+"""
+Unsupervised Learning
+
+reshape image
+"""
 proj = model.fit_transform(vec)
 print(proj.shape)
 proj1=proj.T
 data_fused=proj1.reshape((DR_components,n,k))
 print(np.shape(data_fused))
 
-
+"""
+Save resulting image
+"""
 data_fused_resized=[]
 data_fused_resized.append (cv2.resize(data_fused[0], [n,k], interpolation = cv2.INTER_LANCZOS4))
 print(np.shape(data_fused_resized))    
